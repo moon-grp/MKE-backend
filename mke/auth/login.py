e24f499ba907a25e2cab17937602d08d47c48a6e
emohex <olumidemm@gmail.com>
2020-11-29 20:29:21 +0100

    authentication

--- mke/auth/login.py
+++ mke/auth/login.py
@@ -1,10 +1,36 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, request, Flask
+from flask_pymongo import PyMongo
+
+import bcrypt
+import datetime
+import validators
+from bson import json_util
+from bson.json_util import dumps
+import json
 
 loginEndPoint = Blueprint("loginEndPoint", __name__)
 
 
 @loginEndPoint.route("/signin", methods=["POST"])
 def login():
-    return "e dy work"
+    data = request.json
+    email = "abankab@gmail.com"
+    password = data["password"]
 
+    o_pass = "mrMKEkay"
+    hashed = bcrypt.hashpw(o_pass.encode("utf-8"), bcrypt.gensalt(12))
+    if password:
+        if bcrypt.checkpw(password.encode("utf-8"), hashed):
 
+            access_token = create_access_token(identity={"email": email})
+
+            res = jsonify({
+            "message": "Welcome, mr kay..",
+            "token": access_token
+            })
+
+            return res, 200
+        else:
+            return "wrong password", 400
+    else:
+        return "Enter password!!", 400
--- mke/main.py
+++ mke/main.py
@@ -1,7 +1,13 @@
 from flask import Flask
 from auth.login import loginEndPoint
+from flask_jwt_extended import (
+    JWTManager, jwt_required, create_access_token,
+    get_jwt_identity
+)
 
 app = Flask(__name__)
+app.config['JWT_SECRET_KEY'] = 'mrkayenterpricemrkayframes'  # Change this!
+jwt = JWTManager(app)
 
 app.register_blueprint(loginEndPoint, url_prefix="/api/v1")
 
